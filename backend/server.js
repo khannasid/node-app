ad139b817a5caf9c7c671585c6db22feb431b27a
Siddhant Khanna <khannasiddhant22@gmail.com>
2023-02-18 17:07:41 +0530

    starting-project

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv").config()
+const bodyParser = require("body-parser");
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// connect to DB and start server
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() =>{
+        app.listen(PORT, () =>{
+            console.log(`Server running on port ${PORT}`);
+        })
+    })
+    .catch((err) => console.log(err));
