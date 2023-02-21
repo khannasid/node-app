9ae48ac6e8f924297721bae9a4b51f0c70448aac
Siddhant Khanna <khannasiddhant22@gmail.com>
2023-02-22 01:28:09 +0530

    new routes are added!!

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,14 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bryptjs = require("bcryptjs");
+
 
 const generateToken = (id) => {
     return jwt.sign({id},process.env.JWT_SECRET,{expiresIn:"1d"});
 }
 
-
+//Register User!
 const registerUser = asyncHandler(async (req, res) =>{
         const {name, email, password} = req.body;
 
@@ -57,6 +59,76 @@ const registerUser = asyncHandler(async (req, res) =>{
 
 });
 
+//Login User!!
+const loginUser = asyncHandler(async (req, res) => {
+    
+    const {email, password} = req.body;
+
+    //Validate Request
+    if(!email || !password){
+        res.status(400);
+        throw new Error("Please add email & password");
+    }
+
+    // Chenk if User exists
+    const user = await User.findOne({email});
+
+    if(!user){
+        res.status(400);
+        throw new Error("User not found, Please signup");
+    }
+
+    //User exist, now check if password is correct
+    const passwordIsCorred = await bryptjs.compare(password, user.password);
+    
+    //Generate Token
+    const token = generateToken(user);        
+
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+        path: "/",
+        httpOnly:true,
+        expires:new Date(Date.now()+1000*86400),// 1 Day
+        sameSite:"none",
+        secure:true
+    });
+
+    if(user && passwordIsCorred){
+        const {_id, name, email, photo,phone, bio} = user;
+        res.status(200).json({
+            _id, 
+            name, 
+            email,
+            photo,
+            phone,
+            bio,
+            token
+        });
+    }
+        else{
+            res.status(400);
+            throw new Error("invalid input!!");
+        }
+
+});
+
+// Logout user
+const logout = asyncHandler(async (req, res)=>{
+    // to logout we can delete the cookie or 
+    // expire the cookie that is created while 
+    // login!
+    res.cookie("token","",{
+        path:"/",
+        httpOnly:true,
+        expires: new Date(0),
+        sameSite:"none",
+        secure:true,
+    });
+    return res.status(200).json({
+        message:"Successfully logged out!!"
+    })
+});
+
 module.exports = {
-    registerUser
+    registerUser, loginUser,logout,
 }
